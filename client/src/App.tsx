bac3b4dbb80315f27f9413a69fced0f5f18f11bd
Dan <daniel.smirnov17@gmail.com>
2024-06-06 21:26:11 +0100

    Updated App.tsx with new UI components and refactored API URL; modified vite.config.ts to add environment variables and API proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,34 +3,49 @@ import './App.css'
 import { Button } from './components/ui/button'
 import { Todo } from './types';
 import { Input } from './components/ui/input';
-import { PlusIcon } from 'lucide-react';
+import { CheckCircle2, PlusIcon, Trash } from 'lucide-react';
+import { Badge } from './components/ui/badge';
+import { Shell } from './components/shell';
 
 export default function App() {
-  const BASE_URL = "http://localhost:5000"
-
   // ---------------------------------GET-------------------------------------------
   const { data, isFetching, error: getError } = useQuery<Todo[]>({
     queryKey: ["todos"],
-    queryFn: async () => fetch(`${BASE_URL}/api/todos`).then((res) => res.json()),
+    queryFn: async () => fetch(`/api/todos`).then((res) => res.json()),
   });
 
   (!isFetching && data) && console.log(data)
 
   return (
-    <>
-      <div className="flex w-full max-w-sm items-center space-x-2">
+    <Shell variant={"markdown"}>
+      <div className="flex w-full max-w-md items-center space-x-2">
         <Input type="text" placeholder="Wash Dishes" />
-        <Button type="submit"><PlusIcon /></Button>
+        <Button type="submit">
+          <PlusIcon />
+        </Button>
       </div>
 
+      <br className="mb-20" />
 
-      <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+      <div className="flex flex-col space-y-1">
         {data?.map((item) => (
-          <li key={item._id} className="flex items-center">
-            {item.body}
-          </li>
+          <div key={item._id} className='flex flex-row space-x-2'>
+            <Badge
+              variant="outline"
+              className="rounded-md px-2 max-w-xs flex justify-between w-full mr-1"
+            >
+              {item.body}{" "}
+              {item.completed ? (
+                <span className="bg-green-100">Done</span>
+              ) : (
+                <span className="bg-red-100 px-2 py-0.5">In Progress</span>
+              )}
+            </Badge>
+            <Button variant={'outline'}><CheckCircle2 className='w-5 h-5' /></Button>
+            <Button variant={'outline'}><Trash className='w-5 h-5' /></Button>
+          </div>
         ))}
-      </ul>
-    </>
-  )
+      </div>
+    </Shell>
+  );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,25 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_LOCAL_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};
